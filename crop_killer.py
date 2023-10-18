34af786330767feab10f9192c5913a5790ea8c84
lucas <luckas.cheng@pathmazing.com>
2023-10-18 12:40:26 +0700

    init project

---
+++ crop_killer.py
@@ -0,0 +1,55 @@
+import cv2
+import numpy as np
+import pytesseract
+
+
+async def crop_killer(file_binary_code):
+    pixel_matrix = np.frombuffer(file_binary_code, dtype=np.uint8)
+
+    image = cv2.imdecode(pixel_matrix, cv2.IMREAD_UNCHANGED)
+    # image = cv2.imread(image)
+
+    # Convert the image from BGR to grayscale
+    grayscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Threshold the grayscale image to obtain a binary image
+    _, threshold_image = cv2.threshold(grayscale_image, 200, 255, cv2.THRESH_BINARY)
+
+    # Find contours in the binary image
+    contours, _ = cv2.findContours(threshold_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Find the contour with the largest area (white layout)
+    max_area = 0
+    max_contour = None
+    for contour in contours:
+        area = cv2.contourArea(contour)
+        if area > max_area:
+            max_area = area
+            max_contour = contour
+
+    # Create a bounding rectangle around the white layout contour
+    x, y, w, h = cv2.boundingRect(max_contour)
+
+    # Crop the image using the bounding rectangle
+    cropped_image = image[y:y + h, x:x + w]
+    text = pytesseract.image_to_string(cropped_image).upper()
+
+    keywords = ['USD', 'MAO RITH', 'ABA', 'GO TO ACCOUNTS', 'DONE', '000 354 079']
+
+    if any(keyword in text for keyword in keywords):
+        amount_paid = text.split('USD')[0].split('-')[1]
+
+        _, encoded_image = cv2.imencode('.jpg', cropped_image)
+
+        return encoded_image, amount_paid
+    else:
+        encoded_image = []
+        amount_paid = None
+
+        return encoded_image, amount_paid
+
+    # cv2.imwrite('cropped_image.jpg', cropped_image)
+
+    # _, encoded_image = cv2.imencode('.jpg', cropped_image)
+
+    # return encoded_image
---
+++ main.py
@@ -0,0 +1,32 @@
+import telebot
+from crop_killer import crop_killer
+import os
+import asyncio
+
+bot = telebot.TeleBot(os.getenv('TELEGRAM_TOKEN'))
+
+
+@bot.message_handler(content_types=['photo'])
+def main(message):
+    get_file_path = bot.get_file(message.photo[-1].file_id).file_path  # get file path from telegram bot
+
+    read_file_as_encode = bot.download_file(get_file_path)  # encode file by using file path above
+
+    # is_image_valid = asyncio.run(is_transaction_image(read_file_as_encode))
+    # print(is_image_valid)
+
+    username = f'{message.from_user.first_name} {message.from_user.last_name or ""}'
+    user_message_mention = (message.reply_to_message.text or '') if message.reply_to_message else ''
+
+    encoded_image, amount_paid = asyncio.run(crop_killer(read_file_as_encode))
+
+    if len(encoded_image):
+        bot.delete_message(message.chat.id, message.message_id)  # delete the old recept
+
+        bot.send_photo(message.chat.id, encoded_image, str(message.caption or user_message_mention) + '|' + f'{str(amount_paid)} USD' + ' ' + str(f'({username})'))
+    else:
+        bot.send_message(message.chat.id, 'THIS IS NOT A TRANSACTION RECEIPT !')
+
+
+if __name__ == "__main__":
+    bot.infinity_polling()
