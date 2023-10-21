d6dc13eee5e457d8adcc0e712c30bbc24c3e4f55
lucas <luckas.cheng@pathmazing.com>
2023-10-21 12:17:38 +0700

    feat(): inject image validation

--- crop_killer.py
+++ crop_killer.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import pytesseract
+from color_validation import find_largest_color_area
 
 
 async def crop_killer(file_binary_code):
@@ -12,13 +13,18 @@ async def crop_killer(file_binary_code):
     # Convert the image from BGR to grayscale
     grayscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    # Threshold the grayscale image to obtain a binary image
-    _, threshold_image = cv2.threshold(grayscale_image, 200, 255, cv2.THRESH_BINARY)
+    average_intensity = find_largest_color_area(image)
+
+    if average_intensity > 127:  # if average_intensity bigger than 127 it's mean image area is contained white
+        # Threshold the grayscale image to obtain a binary image
+        _, threshold_image = cv2.threshold(grayscale_image, 200, 255, cv2.THRESH_BINARY)
+    else:
+        _, threshold_image = cv2.threshold(grayscale_image, 50, 255, cv2.THRESH_BINARY_INV)
 
     # Find contours in the binary image
     contours, _ = cv2.findContours(threshold_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    # Find the contour with the largest area (white layout)
+    # Find the contour with the largest area (white or black layout)
     max_area = 0
     max_contour = None
     for contour in contours:
@@ -34,7 +40,7 @@ async def crop_killer(file_binary_code):
     cropped_image = image[y:y + h, x:x + w]
 
     text = pytesseract.image_to_string(cropped_image).upper()
-    # print({'detail': text})
+    print({'detail': text})
 
     keywords = ['USD', 'MAO RITH', 'RITH MAO', 'ABA', 'GO TO ACCOUNTS', 'DONE', '000 354 079']
 
@@ -45,7 +51,7 @@ async def crop_killer(file_binary_code):
         if 'USP' in text:
             amount_paid = text.split('USP')[0].split('-')[1]
 
-        # print({'user_transaction': amount_paid})
+        print({'user_transaction': amount_paid})
 
         _, encoded_image = cv2.imencode('.jpg', cropped_image)
 
